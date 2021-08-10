a2a87206318994e3f209fd6f24af0527dd89be5e
dg12988 <doug.goldberg@me.com>
2021-08-10 02:58:03 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 //Mike is from New Delhi and likes to eat pasta. 
 
+import { forkJoin } from "rxjs";
 import { ajax } from "rxjs/ajax";
 
 const randomName$ = ajax
@@ -11,7 +12,10 @@ const randomNation$ = ajax
 const randomFood$ = ajax
 ('https://random-data-api.com/api/food/random_food');
 
-randomName$.subscribe(ajaxResponse => console.log(ajaxResponse.response.first_name));
-randomNation$.subscribe(ajaxResponse => console.log(ajaxResponse.response.capital));
-randomFood$.subscribe(ajaxResponse => console.log(ajaxResponse.response.dish));
+// randomName$.subscribe(ajaxResponse => console.log(ajaxResponse.response.first_name));
+// randomNation$.subscribe(ajaxResponse => console.log(ajaxResponse.response.capital));
+// randomFood$.subscribe(ajaxResponse => console.log(ajaxResponse.response.dish));
 
+forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
+  (ajaxRepsonses) => console.log(ajaxRepsonses)
+);
